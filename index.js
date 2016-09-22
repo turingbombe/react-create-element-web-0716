18fda5b22767c4dfa5e1e16720082d770be2c129
Charlie Cook <turingbombe@gmail.com>
2016-09-22 14:33:24 -0400

    Completed react create element code along

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const list =
     React.createElement('h1', {}, 'My favorite ice cream flavors'),
     React.createElement('ul', {},
       [
-        React.createElement('li', { class: 'brown' }, 'Chocolate'),
-        React.createElement('li', { class: 'white' }, 'Vanilla'),
-        React.createElement('li', { class: 'yellow' }, 'Banana')
+        React.createElement('li', { className: 'brown' }, 'Chocolate'),
+        React.createElement('li', { className: 'white' }, 'Vanilla'),
+        React.createElement('li', { className: 'yellow' }, 'Banana')
       ]
     ));
+const title = React.createElement('h1', {}, 'My First React Code');
+const paragraph = React.createElement('p', {}, 'Writing some more HTML. Cool stuff!');
+const container = React.createElement('div', {}, [title, paragraph]);
+ 
+ReactDOM.render(
+  container,
+  document.getElementById('main')
+);
 
-ReactDOM.render(list, document.getElementById('main'));
